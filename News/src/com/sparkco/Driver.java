81da47508d6288196649c62e4c2503f7b0b93ebb
ru-sparks <ru_sparks@yahoo.com>
2022-09-17 18:35:29 -0700

    initial commit

---
+++ News/src/com/sparkco/Driver.java
@@ -0,0 +1,10 @@
+package com.sparkco;
+
+public class Driver {
+
+  public static void main(String[] args) {
+    Utility.sayHi();
+
+  }
+
+}
---
+++ News/src/com/sparkco/Utility.java
@@ -0,0 +1,7 @@
+package com.sparkco;
+
+public class Utility {
+  public static void sayHi() {
+    System.out.println("Hi");
+  }
+}
